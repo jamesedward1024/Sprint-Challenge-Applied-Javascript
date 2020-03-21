8ce41d66035ff9eaee9ce1116f087b2317583dd4
James Leyden <jamesedward1024@gmail.com>
2020-03-21 12:12:30 -0500

    mvp

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,19 +1,61 @@
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to write a function that creates the carousel component, you will find the HTML below.
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+function createCarousel() {
+  let carousel = document.createElement("div");
+  let leftBtn = document.createElement("div");
+  let image = document.createElement("img");
+  let rightBtn = document.createElement("div");
 
-/* HTML:
-  <div class="carousel">
-    <div class="left-button"> < </div>
-    <img src="./assets/carousel/mountains.jpeg" />
-    <img src="./assets/carousel/computer.jpeg" />
-    <img src="./assets/carousel/trees.jpeg" />
-    <img src="./assets/carousel/turntable.jpeg" />
-    <div class="right-button"> > </div>
-  </div>
-*/
+  carousel.classList.add("carousel");
+  leftBtn.classList.add("left-button");
+  rightBtn.classList.add("right-button");
+  image.classList.add("current-img");
+
+  image.src = "./assets/carousel/mountains.jpeg";
+
+  carousel.append(leftBtn, image, rightBtn);
+
+  return carousel;
+}
+
+const carouselContainer = document.querySelector(".carousel-container");
+carouselContainer.append(createCarousel());
+
+const leftButton = document.querySelector(".left-button");
+const rightButton = document.querySelector(".right-button");
+let image = document.querySelector(".current-img");
+// images src array
+const images = [
+  "./assets/carousel/mountains.jpeg",
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg"
+];
+
+let currentIndex = 0;
+
+function nextImage() {
+  if (currentIndex < images.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  image.src = images[currentIndex];
+}
+
+rightButton.addEventListener("click", e => {
+  nextImage();
+  e.stopPropagation();
+});
+
+function prevImage() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = images.length - 1;
+  }
+  image.src = images[currentIndex];
+}
+
+leftButton.addEventListener("click", e => {
+  prevImage();
+  e.stopPropagation();
+});
